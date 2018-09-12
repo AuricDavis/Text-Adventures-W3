50abad128534ddf64b2b73adeeb8d9380bb0d149
Merit Student <student@meac.local>
2018-09-12 13:31:10 -0600

    wHoop di do

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-print("To start, type \"start\"")
+print("To start the game, type \"start\".\nTo quit the game, type \"quit\"")
 fromUser = input()
 while True:
     if fromUser == "start":
         print("Get ready for your adventure!")
+        
+    elif fromUser == "quit":
+        print("Okay, I wanted to be alone anyway!")
+        break
     else:
         print("... You're not very bright, are you?")
+    fromUser = input()
