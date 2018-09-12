84893328fbc6bb548bcdbd3ac9fa0f2327b6e257
Merit Student <student@meac.local>
2018-09-12 13:16:03 -0600

    hi

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Working!")
+print("To start, type \"start\"")
+fromUser = input()
+while True:
+    if fromUser == "start":
+        print("Get ready for your adventure!")
+    else:
+        print("... You're not very bright, are you?")
